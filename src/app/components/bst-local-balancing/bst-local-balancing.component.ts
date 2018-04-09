cc85fd146badf6f3364253bfa7df1c0fd751197b
v.butko <v.butko@emict.net>
2018-04-10 01:16:45 +0300

    HTML developing - palyer algo page

--- src/app/components/bst-local-balancing/bst-local-balancing.component.ts
+++ src/app/components/bst-local-balancing/bst-local-balancing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bst-local-balancing',
@@ -7,9 +8,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BstLocalBalancingComponent implements OnInit {
 
-  constructor() { }
+  BST_MAX_START_ITEMS = 20;
+  BST_MAX_ITEMS = 30;
+  state: any[] = [];
+
+  playingMode = 'nonstop';
+  itemsStart = [];
+  bstItems: any = [];
+
+  constructor(
+    private router: Router) {
+    let i = 0;
+    while (i <= this.BST_MAX_START_ITEMS) {
+      const item = { display: 'inline-block', id: i, value: '' + i };
+      this.itemsStart.push(item);
+      i++;
+    }
+    i = 0;
+    while (i <= this.BST_MAX_ITEMS) {
+      const item = { display: 'none', id: i, value: '-' };
+      this.bstItems.push(item);
+      i++;
+    }
+  }
 
   ngOnInit() {
   }
 
+  /**
+   *
+   * @constructor
+   */
+  AddElement() {
+    console.log('Start add element');
+    let i = 0;
+    const max = this.itemsStart.length;
+    while (i <= max) {
+      if (this.itemsStart[i].display === 'inline-block') {
+        console.log('add', i);
+        this.itemsStart[i].display = 'none';
+        this.bstItems[i].display = 'block';
+        this.bstItems[i].value = this.itemsStart[i].value;
+        break;
+      }
+      i++;
+    }
+  }
+
+  RemoveElement() {
+    console.log('Start remove element');
+    let i = 0;
+    const max = this.itemsStart.length;
+    while (i <= max) {
+      if (this.itemsStart[i].display === 'inline-block') {
+        console.log('add', i);
+        this.itemsStart[i].display = 'none';
+        this.bstItems[i].display = 'block';
+        this.bstItems[i].value = this.itemsStart[i].value;
+        break;
+      }
+      i++;
+    }
+  }
+
+  BalanceRandom() {
+    console.log('Start BalanceRandom');
+  }
+
+  BalanceAmortization() {
+    console.log('Start BalanceAmortization');
+  }
+
+  BalanceOptimization() {
+    console.log('Start BalanceOptimization');
+  }
+
+  PlayNonStop() {
+    console.log('Start PlayNonStop');
+  }
+
+  PlayStepByStep() {
+    console.log('Start PlayStepByStep');
+  }
+
+  GetInfo() {
+    console.log('Start GetInfo');
+  }
+
+  RouteStart() {
+    this.router.navigate(['start']);
+  }
 }
